0cf0e2f13ba23baafe989037b88ba6c0604d6480
Marc Bakker <webdev@marcbakker.com>
2023-12-03 01:20:49 +0100

    Toon popup window met text.

--- src/app/components/hottext/dynamictext.component.ts
+++ src/app/components/hottext/dynamictext.component.ts
@@ -6,6 +6,7 @@ import {
   HostListener,
   OnInit,
   Renderer2,
+  ViewChild,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -16,31 +17,78 @@ import { CommonModule } from '@angular/common';
   templateUrl: './dynamictext.component.html',
   styleUrls: ['./dynamictext.component.scss'],
 })
-export class DynamicTextComponent implements OnInit, AfterViewInit {
+export class DynamicTextComponent implements AfterViewInit {
+  @ViewChild('elementHost')
+  elementHost!: any;
+  popup!: any;
+  dynamicTextAlternatives!: { [key: string]: string[] };
+
   @HostBinding('attr.class') cssClass!: string;
-  public hottext: string = '';
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
-
-  ngOnInit() {
-    this.renderer.addClass(this.elementRef.nativeElement, 'textContainer');
-  }
+  constructor(private renderer: Renderer2) {}
 
   ngAfterViewInit() {
-    setTimeout(() => {
-      this.hottext = this.elementRef.nativeElement.textContent;
-      this.elementRef.nativeElement.innerHTML = this.hottext;
-    }, 0);
+    this.initializePopupWindowTexts();
+    this.createPopUpWindow();
+  }
+
+  initializePopupWindowTexts() {
+    this.dynamicTextAlternatives = {
+      OpenAI: [
+        'DeepMind',
+        'Usage',
+        'IBM Watson',
+        'Microsoft Azure',
+        'Google Cloud AI',
+        'Amazon Machine Learning',
+        'NVIDIA DGX',
+        'Intel AI',
+        'Apple Core ML',
+        'H2O.ai',
+        'OpenCV',
+      ],
+    };
   }
 
   @HostListener('click', ['$event'])
-  showPopup(event: any) {
-    console.log(
-      event.target.dataset.hottext == ''
-        ? 'U clicked op een hot woord'
-        : 'Geen hot woord...'
+  onHotWordClick(event: any) {
+    if (
+      Object.keys(this.dynamicTextAlternatives).includes(
+        event.target.dataset.hottext
+      )
+    ) {
+      this.hidePopup();
+      this.updatePopupText(event.target.dataset.hottext);
+      this.showPopup({ posX: event.clientX, posY: event.clientY });
+    }
+  }
+
+  showPopup(location: { posX: number; posY: number }) {
+    this.renderer.setStyle(this.popup, 'top', location.posY + 'px');
+    this.renderer.setStyle(this.popup, 'left', location.posX + 'px');
+    this.renderer.addClass(this.popup, 'display');
+  }
+
+  hidePopup() {
+    this.renderer.removeClass(this.popup, 'display');
+    Array.from(this.popup.childNodes).forEach((childNode) =>
+      this.renderer.removeChild(this.popup, childNode)
     );
-    // e.srcElement.innerHTML =
-    //   e.srcElement.innerHTML == 'World' ? 'Marc' : 'World';
+  }
+
+  createPopUpWindow() {
+    this.popup = this.renderer.createElement('div');
+    this.renderer.addClass(this.popup, 'popupwindow');
+    this.renderer.listen(this.popup, 'click', () => this.hidePopup());
+    this.renderer.appendChild(this.elementHost.nativeElement, this.popup);
+  }
+
+  updatePopupText(clickedWord: string) {
+    this.dynamicTextAlternatives[clickedWord].forEach((textAlternative) => {
+      const paragraph = this.renderer.createElement('p');
+      const text = this.renderer.createText(textAlternative);
+      this.renderer.appendChild(paragraph, text);
+      this.renderer.appendChild(this.popup, paragraph);
+    });
   }
 }
